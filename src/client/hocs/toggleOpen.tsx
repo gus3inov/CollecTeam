357113811988fc2d773816ca0693624951f0de5c
Muslim Guseinov <gusejnov1999@list.ru>
2018-05-06 02:28:20 +0300

    @update(toggleOpen) create toggle component

---
+++ src/client/hocs/toggleOpen.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import {Fragment} from "react"
+
+export interface InjectedProps {
+    isOpen: boolean;
+    toggleOpen(): any;
+}
+
+export interface ExternalProps {
+    isOpen: boolean;
+}
+
+export interface State {
+    isOpen: boolean;
+}
+
+export default function toggleOpen<OriginProps>(Component: React.ComponentType<OriginProps & InjectedProps>){
+    type ResultProps = OriginProps & ExternalProps;
+    type Component = React.ComponentType;
+
+    return class extends React.Component<ResultProps, State>{
+
+        static displayName =  `toggleOpen(${ Component.displayName })`;
+
+        state: State = {
+            isOpen: this.props.isOpen
+        };
+
+        toggleOpen = () => {
+            this.setState({
+                isOpen: !this.state.isOpen
+            })
+        };
+
+        render(){
+            return (
+                <Fragment>
+                    <Component {...this.props} {...this.state} toggleOpen = { this.toggleOpen } />;
+                </Fragment>
+            );
+        }
+
+    }
+}
