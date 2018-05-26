3aa438c4740dee346a3fe9e4ebf812d0d018d2f8
Muslim Guseinov <gusejnov1999@list.ru>
2018-05-26 14:02:04 +0300

    @update(clinent routing)

--- src/client/components/Root.tsx
+++ src/client/components/Root.tsx
@@ -1,23 +1,63 @@
 import * as React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import {Route, Redirect, Switch} from 'react-router-dom';
+import {connect} from "react-redux";
 
+import {isAuth} from '../ducks/auth'
 import AuthPage from './routes/AuthPage';
 import NotFound from './routes/NotFound';
 import HomePage from './routes/HomePage';
+import AuthService from '../services/AuthService'
 
 export interface RootProps {
 
 }
 
+const PrivateRoute = ({component: Component, isAuth, ...rest}) => (
+    <Route {...rest} render={props => (
+        isAuth ? (
+            <Component {...props} {...rest} />
+        ) : (
+            <Redirect to={{
+                pathname: '/auth',
+                state: {from: props.location}
+            }}/>
+        )
+    )}/>
+)
+
+@connect(null, {isAuth})
 class Root extends React.Component<RootProps, any> {
+    state = {
+        isAuthenticate: false
+    }
+
+    componentDidMount() {
+        const {isAuth} = this.props;
+        isAuth();
+
+        this.setState({
+            isAuthenticate: AuthService.isUserAuthenticated()
+        })
+    }
+
     render() {
+        const {isAuthenticate} = this.state;
+        console.log(isAuthenticate)
         return (
             <div>
                 {/*<Redirect to="/auth/signup" />*/}
                 <Switch>
-                    <Route path="/home" component={ HomePage } />
-                    <Route path="/auth" component={ AuthPage } />
-                    <Route path="*" component={ NotFound } />
+                    <Route exact path="/" render={props => (
+                        isAuthenticate ? (
+                            <Redirect to='/home'/>
+                        ) : (
+                            <Redirect to='/auth'/>
+                        )
+                    )}/>
+                    {/*<PrivateRoute exact path="/" isAuth={isAuthenticate} component={HomePage}/>*/}
+                    <Route path="/auth" component={AuthPage}/>
+                    <Route path="/home" component={HomePage}/>
+                    <Route path="*" component={NotFound}/>
                 </Switch>
             </div>
         );
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import  './css/bootstrap.css';
-import './css/main.css';
+import './client/css/bootstrap.css';
+import './client/css/main.css';
 import App from './client/App';
 import {ConnectedRouter} from 'react-router-redux';
 import {Provider} from 'react-redux';
 import history from './client/helpers/history';
 import store from './client/redux';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+const supportsHistory = 'pushState' in window.history
 
 ReactDOM.hydrate(
-    <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-    </Provider>,
+    <Router forceRefresh={!supportsHistory}>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </Router>,
     document.getElementById('root') as HTMLElement
 );
