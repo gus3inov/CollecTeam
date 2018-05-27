a489f23570f06412b71d084331fd5bf90577aedf
Muslim Guseinov <gusejnov1999@list.ru>
2018-05-28 01:09:01 +0300

    @update(font) adding fonr Algereya SC

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -71,6 +71,7 @@ const renderHTML = (componentHTML: any) => {
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link href="//cdn.materialdesignicons.com/2.3.54/css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />
+          <link href="https://fonts.googleapis.com/css?family=Alegreya+SC" rel="stylesheet">
           <title>Collect Team</title>
       </head>
       <body>
