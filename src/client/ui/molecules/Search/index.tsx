bc0fa0ebca0497b94dbc0de51ede8f5d844a543c
Muslim Guseinov <gusejnov1999@list.ru>
2018-05-28 01:17:00 +0300

    @update(molecules) adding new molecules

--- src/client/ui/molecules/Search/index.tsx
+++ src/client/ui/molecules/Search/index.tsx
@@ -1,14 +1,3 @@
-import * as React from 'react';
-
-import MainSearch from './Search';
-
-export interface HeaderProps {
-
-}
-
-const Search:  React.StatelessComponent<HeaderProps> = (props: HeaderProps) => {
-
-    return <MainSearch />
-};
+import Search from './Search';
 
 export default Search;
