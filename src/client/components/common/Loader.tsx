e25c75d4343ac5dc05fb1780c0ba1b5117cf8b35
Muslim Guseinov <gusejnov1999@list.ru>
2018-06-03 23:54:44 +0300

    @update(loader) loader from material

--- src/client/components/common/Loader.tsx
+++ src/client/components/common/Loader.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => ({
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
 
 type LoaderProps = {
 
 }
+
 const Loader: React.SFC<LoaderProps> = (props) => {
-    return <h2>Loading...</h2>
+    const { classes } = props;
+    return <CircularProgress className={classes.progress} size={75} />
 }
 
-export default Loader;
+export default withStyles(styles)(Loader);
