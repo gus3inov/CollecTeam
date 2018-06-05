b23198b9e34cd6071a695b73162e41899a078360
Muslim Guseinov <gusejnov1999@list.ru>
2018-06-06 00:37:22 +0300

    @update(startup) startup list

--- src/client/components/Home/Startups.tsx
+++ src/client/components/Home/Startups.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import {NavLink} from 'react-router-dom';
+import {connect} from 'react-redux';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Loader from '../common/Loader';
+import Slide from '@material-ui/core/Slide';
 
 import Section from '../../ui/organisms/Section';
-import { loadAllStartups, moduleName } from '../../ducks/startups';
+import {loadAllStartups, moduleName} from '../../ducks/startups';
 
 
 export interface StartupsProps {
@@ -32,53 +34,69 @@ const styles = {
         loading: state[moduleName].loading,
         startups: state[moduleName].entities
     };
-}, { loadAllStartups })
+}, {loadAllStartups})
 @withStyles(styles)
 class Startups extends React.Component<StartupsProps, any> {
 
-    componentDidMount () {
+    componentDidMount() {
         const {loadAllStartups} = this.props;
 
         loadAllStartups();
     }
 
     render() {
-        const { classes, startups } = this.props;
+        const {classes, startups, loading} = this.props;
 
-        console.log(startups)
         return (
             <Section title="Стартапы">
-                <div className="startups-list">
-                    {
-                        startups.map(startup => {
-                            return (
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.media}
-                                        image={startup.previewPicture}
-                                        title={startup.name}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="headline" component="h2">
-                                            {startup.name}
-                                        </Typography>
-                                        <Typography component="p">
-                                            {startup.description}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="primary">
-                                            Поделиться
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Узнать подробнее
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            )
-                        })
-                    }
-                </div>
+                {
+                    loading
+                        ?
+                        <Loader/>
+                        :
+                        <div className="startups-list">
+                            {
+                                startups.map((startup, index) => {
+                                    return (
+                                        <Slide
+                                            direction="up"
+                                            key={startup.id}
+                                            in={!loading}
+                                            timeout={(1000 + (index * 2))}
+                                            style={{ transformOrigin: '0 0 0' }}
+                                              {...(!loading ? { transitionDelay: (10000 + (index * 10)) } : {})}
+                                        >
+                                            <Card className={classes.card}>
+                                                <CardMedia
+                                                    className={classes.media}
+                                                    image={startup.previewPicture}
+                                                    title={startup.name}
+                                                />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="headline" component="h2">
+                                                        {startup.name}
+                                                    </Typography>
+                                                    <Typography component="p">
+                                                        {startup.description}
+                                                    </Typography>
+                                                </CardContent>
+                                                <CardActions>
+                                                    <Button size="small" color="primary">
+                                                        Поделиться
+                                                    </Button>
+                                                    <NavLink to={`/home/startups/${startup.name}`}>
+                                                        <Button size="small" color="primary">
+                                                            Узнать подробнее
+                                                        </Button>
+                                                    </NavLink>
+                                                </CardActions>
+                                            </Card>
+                                        </Slide>
+                                    );
+                                })
+                            }
+                        </div>
+                }
             </Section>
         );
     }
