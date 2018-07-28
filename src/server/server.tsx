134721f7a8176b010dd06fafaffac91d816585ef
Muslim Guseinov <gusejnov1999@list.ru>
2018-07-28 20:11:33 +0300

    @update(server) change imports, refactor code

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -10,22 +10,22 @@ import * as logger from 'koa-logger';
 import { renderRoutes } from 'react-router-config';
 import * as cookiesMiddleware from 'universal-cookie-koa';
 import { CookiesProvider } from 'react-cookie';
-import { cookies } from '../client/helpers/cookies';
+import { cookies } from '@client/helpers/cookies';
 import * as serve from 'koa-static';
 
-import err from './middlewares/error';
-import UserController from './controllers/UserController';
-import User from './models/User';
+import err from '@server/middlewares/error';
+import UserController from '@server/controllers/UserController';
+import User from '@server/models/User';
 
-import StartupController from './controllers/StartupController';
-import Startup from './models/Startup';
+import StartupController from '@server/controllers/StartupController';
+import Startup from '@server/models/Startup';
 
-import TeamController from './controllers/TeamController';
-import Team from './models/Team';
+import TeamController from '@server/controllers/TeamController';
+import Team from '@server/models/Team';
 
-import passportInit from './libs/passport'
-import store from '../client/redux';
-import routes from '../client/components/routes';
+import passportInit from '@server/libs/passport'
+import store from '@client/redux';
+import routes from '@client/components/routes';
 import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
 
 const serverPort = config.get('dev.serverPort');
@@ -39,12 +39,12 @@ app.keys = ['your-session-secret'];
 app.use(cookiesMiddleware());
 
 app.use(session({
-    store: new RedisStore()
+	store: new RedisStore()
 }, app));
 
 app.use(serve(__dirname + '\\public'));
 
-import './authenticate/init';
+import '@server/authenticate/init';
 
 passportInit(app);
 
@@ -66,50 +66,48 @@ const teamController = new TeamController(teamModel);
 app.use(teamController.getRoutes());
 app.use(teamController.getMethods());
 
-import { blue, deepPurple, grey } from '@material-ui/core/colors';
+import {blue, deepPurple, grey} from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: blue,
-        primary1Color: blue,
-        primary2Color: "#2173B3",
-        primary3Color: "#A9D2EB",
-        accent1Color: "#ED3B3B",
-        accent2Color: "#ED2B2B",
-        accent3Color: "#F58C8C",
-        accent: deepPurple,
-        type: 'dark',
-        textColor: grey[50],
-        alternateTextColor: grey[50],
-    }
+	palette: {
+		primary: blue,
+		primary2Color: '#2173B3',
+		primary3Color: '#A9D2EB',
+		accent1Color: '#ED3B3B',
+		accent2Color: '#ED2B2B',
+		accent3Color: '#F58C8C',
+		accent: deepPurple,
+		type: 'dark',
+		alternateTextColor: grey[50],
+	},
 });
 
 app.use(async (ctx, next) => {
-    const context = {};
+	const context = {};
 
-    const componentHTML = ReactDomServer.renderToString(
-        <StaticRouter location={ctx.request.url} context={context}>
-                            <Provider store={store}>
-                                <CookiesProvider cookies={ctx.request.universalCookies}>
-                                    <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
-                                        {renderRoutes(routes)}
-                                    </MuiThemeProvider>
-                                </CookiesProvider>
-                            </Provider>
-                </StaticRouter>
-    );
+	const componentHTML = ReactDomServer.renderToString(
+		<StaticRouter location={ctx.request.url} context={context}>
+			<Provider store={store}>
+				<CookiesProvider cookies={ctx.request.universalCookies}>
+					<MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+						{renderRoutes(routes)}
+					</MuiThemeProvider>
+				</CookiesProvider>
+			</Provider>
+		</StaticRouter>
+	);
 
-    cookies.setCookies(ctx.request.universalCookies);
+	cookies.setCookies(ctx.request.universalCookies);
 
-    if (context.url) {
-        ctx.response.redirect(context.url)
-    }
+	if (context.url) {
+		ctx.response.redirect(context.url);
+	}
 
-    return ctx.res.end(renderHTML(componentHTML));
+	return ctx.res.end(renderHTML(componentHTML));
 });
 
 const renderHTML = (componentHTML: any) => {
-    return `
+	return `
     <!DOCTYPE html>
       <html>
       <head>
