e7435cbd7af2d8a9adc33bc991f53f41e1833de2
Muslim Guseinov <gusejnov1999@list.ru>
2018-07-28 20:13:14 +0300

    @refactor(controllers) change imports, code style refactor

--- src/server/controllers/TeamController.ts
+++ src/server/controllers/TeamController.ts
@@ -1,85 +1,82 @@
+import Router from '@core/RouteGenerator';
+import { ITeamModel } from '@server/models/Team';
+import { Context, ErrorStatus, Next } from '@server/interfaces/IKoa';
+import checkAuth from '@server/middlewares/checkAuth';
 import * as convert from 'koa-convert';
 import * as KoaBody from 'koa-body';
 import * as bodyParser from 'koa-bodyparser';
-
-import Router from '../helpers/RouteGenerator';
-import { ITeamModel } from '../models/Team';
-import { ContextStats, ErrorStatus, IRequest } from '../interfaces/IKoa';
-import checkAuth from '../middlewares/checkAuth';
-
-type RequestStats = IRequest;
 
 const koaBody = convert(KoaBody());
 
 class TeamController extends Router {
 
-    private model: ITeamModel;
+	private model: ITeamModel;
 
-    constructor(model: ITeamModel){
-        super();
-        this.model = model;
-        this.actionCreate();
-        this.actionGetAll();
-        this.actionGet();
-        this.actionChange();
-        this.actionDelete();
-    }
+	constructor(model: ITeamModel) {
+		super();
+		this.model = model;
+		this.actionCreate();
+		this.actionGetAll();
+		this.actionGet();
+		this.actionChange();
+		this.actionDelete();
+	}
 
-    public getRoutes (): any {
-        return this.listenRoutes();
-    }
+	public getRoutes(): any {
+		return this.listenRoutes();
+	}
 
-    public getMethods (): any {
-        return this.allowedMethods();
-    }
+	public getMethods(): any {
+		return this.allowedMethods();
+	}
 
-    public actionCreate () {
-        this.post('/api/team', koaBody, checkAuth, async(ctx: ContextStats, next: any) => {
-            ctx.status = ErrorStatus.Created;
-            console.log(ctx.request.body)
-            const team = await this.model.create(ctx.request.body);
+	public actionCreate() {
+		this.post('/api/team', koaBody, checkAuth, async (ctx: Context, next: Next) => {
+			ctx.status = ErrorStatus.Created;
+			const team = await this.model.create(ctx.request.body);
+			ctx.body = team;
+			await next();
+		});
+	}
 
-            ctx.body = team;
-        })
-    }
+	public actionGetAll() {
+		this.get('/api/teams', bodyParser(), checkAuth, async (ctx: Context, next: Next) => {
+			const result: Array<object> = await this.model.getAll();
+			ctx.body = result[0];
+			await next();
+		});
+	}
 
-    public actionGetAll () {
-        this.get('/api/teams', bodyParser(), checkAuth, async(ctx: ContextStats, next: any) => {
-            const userId = ctx.body.user.id;
-            console.log(userId);
-            const result: Array<object> = await this.model.getAll(userId);
-            ctx.body = result[0];
-            console.log(ctx.body)
-        });
-    }
+	public actionGet() {
+		this.get('/api/team/:name', bodyParser(), checkAuth, async (ctx: Context, next: Next) => {
+			const result: Array<object> = await this.model.findByTeamName(ctx.params.name);
 
-    public actionGet () {
-        this.get('/api/team/:name', bodyParser(), checkAuth, async(ctx: ContextStats, next: any) => {
-            let result: Array<object> = await this.model.findByTeamName(ctx.params.name);
+			if (result) {
+				ctx.body = result[0];
+			} else {
+				ctx.status = ErrorStatus.NoContent;
+			}
+			await next();
+		});
+	}
 
-            if (result) {
-                ctx.body = result[0];
-            } else {
-                ctx.status = ErrorStatus.NoContent;
-            }
-        });
-    }
+	public actionChange() {
+		this.put('/api/team/edit', bodyParser(), checkAuth, async <RequestStats>(ctx: Context, next: Next) => {
+			ctx.status = ErrorStatus.NoContent;
 
-    public actionChange () {
-        this.put('/api/team/edit', bodyParser(), checkAuth, async <RequestStats> (ctx: ContextStats, next: any) => {
-            ctx.status = ErrorStatus.NoContent;
+			await this.model.update(ctx.params.name, ctx.request.body);
+			await next();
+		});
+	}
 
-            await this.model.update(ctx.params.name, ctx.request.body);
-        })
-    }
+	public actionDelete() {
+		this.delete('/api/team/delete', bodyParser(), checkAuth, async (ctx: Context, next: Next) => {
+			ctx.status = ErrorStatus.NoContent;
 
-    public actionDelete () {
-        this.delete('/api/team/delete', bodyParser(), checkAuth, async (ctx: ContextStats, next: any) => {
-            ctx.status = ErrorStatus.NoContent;
-
-            await this.model.remove(ctx.params.id);
-        })
-    }
+			await this.model.remove(ctx.params.id);
+			await next();
+		});
+	}
 
 }
 
--- src/server/controllers/UserController.ts
+++ src/server/controllers/UserController.ts
@@ -1,178 +1,187 @@
+import Router from '@core/RouteGenerator';
+import checkAuth from '@server/middlewares/checkAuth';
+import { Context, ErrorStatus, Next } from '@server/interfaces/IKoa';
 import * as convert from 'koa-convert';
 import * as KoaBody from 'koa-body';
 import * as passport from 'passport';
 import * as bodyParser from 'koa-bodyparser';
 
-import Router from '../helpers/RouteGenerator';
-import { IUserModel } from '../models/User';
-import checkAuth, {default as checkAuth} from '../middlewares/checkAuth';
-import { ContextStats, ErrorStatus, IRequest } from '../interfaces/IKoa';
-
-type RequestStats = IRequest;
-
 const koaBody = convert(KoaBody());
-
-/***
- * @todo Сделать MV подобную архитектуру с контроллером, в качестве аргумента инстанса принемаемый экземлпяр модели
- */
 
 class UserController extends Router {
 
-    private model: IUserModel;
+	private model: IUserModel;
 
-    constructor(model: IUserModel){
-        super();
-        this.model = model;
-        this.setToken();
-        this.getToken();
-        this.deleteToken();
-        this.actionCreate();
-        this.actionGetAll();
-        this.actionGet();
-        this.actionChange();
-        this.actionDelete();
-        this.actionLogin();
-        this.actionLogout();
-        this.isAuth();
-    }
+	constructor(model: IUserModel) {
+		super();
+		this.model = model;
+		this.setToken();
+		this.getToken();
+		this.deleteToken();
+		this.actionCreate();
+		this.actionGetAll();
+		this.actionGet();
+		this.actionChange();
+		this.actionDelete();
+		this.actionLogin();
+		this.actionLogout();
+		this.isAuth();
+	}
 
-    public getRoutes (): any {
-        return this.listenRoutes();
-    }
+	public getRoutes(): any {
+		return this.listenRoutes();
+	}
 
-    public getMethods (): any {
-        return this.allowedMethods();
-    }
+	public getMethods(): any {
+		return this.allowedMethods();
+	}
 
-    public actionCreate () {
-        this.post('/api/user', koaBody, async(ctx: ContextStats, next: any) => {
-            console.log(ctx.request.body)
-            ctx.status = ErrorStatus.Created;
-            const user = await this.model.create(ctx.request.body);
+	public actionCreate() {
+		this.post('/api/user', koaBody, async (ctx: Context) => {
+			ctx.status = ErrorStatus.Created;
+			const userModel = await this.model.create(ctx.request.body);
 
-            ctx.body = user;
+			ctx.body = userModel;
 
-            return passport.authenticate('local', (err, token, user) => {
-                if (user) {
-                    ctx.body = { success: true, token };
-                    return ctx.login(user);
-                } else {
-                    ctx.status = 400;
-                    ctx.body = {
+			return passport.authenticate('local', async (err, token, user) => {
+				if (user) {
+					ctx.body = {success: true, token};
 
-                        error: err
-                    };
-                }
-            })(ctx);
-        })
-    }
+					return ctx.login(user);
+				} else {
+					ctx.status = 400;
+					ctx.body = {
+						error: err,
+					};
 
-    public actionLogin () {
-        this.post('/auth/login', bodyParser(), async (ctx: ContextStats) => {
-            return passport.authenticate('local', (err, token, user) => {
-                if (user) {
-                    const userRequest = {
-                        id: user.id,
-                        username: user.username,
-                        firstName: user.first_name,
-                        lastName: user.last_name,
-                        email: user.email,
-                        avatar: user.avatar,
-                        position: user.position_id,
-                        role: user.role_id,
-                        team: user.team_id
-                    };
+					return err;
+				}
+			})(ctx);
+		});
+	}
 
-                    delete user.salt;
-                    delete user.password;
-                    ctx.body = { success: true, token, user: userRequest};
-                    return ctx.login(user);
-                } else {
-                    ctx.status = 400;
-                    ctx.body = {
-                        error: `user : ${JSON.stringify(err)}`
-                    };
-                }
-            })(ctx);
-        });
-    }
+	public actionLogin() {
+		this.post('/auth/login', bodyParser(), async (ctx: Context) => {
+			return passport.authenticate('local', (err, token, user) => {
+				if (user) {
+					const userRequest = {
+						id: user.id,
+						username: user.username,
+						firstName: user.first_name,
+						lastName: user.last_name,
+						email: user.email,
+						avatar: user.avatar,
+						position: user.position_id,
+						role: user.role_id,
+						team: user.team_id,
+					};
 
-    public isAuth () {
-        this.get('/auth/isauth', bodyParser(), checkAuth, async (ctx, next) => {
-            const user = ctx.body.user;
-            ctx.body = {
-                user
-            }
-        });
-    }
+					delete user.salt;
+					delete user.password;
+					ctx.body = {success: true, token, user: userRequest};
+					return ctx.login(user);
+				} else {
+					ctx.status = 400;
+					ctx.body = {
+						error: `user : ${JSON.stringify(err)}`,
+					};
 
-    public actionLogout () {
-        this.get('/auth/logout', bodyParser(), async (ctx) => {
-            if (ctx.isAuthenticated()) {
-                await ctx.cookies.set('token', null);
+					return err;
+				}
+			})(ctx);
+		});
+	}
 
-                return ctx.logout();
-            } else {
-                ctx.body = { success: false };
-                ctx.throw(401);
-            }
-        });
-    }
+	public isAuth() {
+		this.get('/auth/isauth', bodyParser(), checkAuth, async (ctx, next: Next) => {
+			const user = ctx.body.user;
+			ctx.body = {
+				user,
+			};
 
-    public actionGetAll () {
-        this.get('/api/users', async(ctx: ContextStats, next: any) => {
-            const result: Array<object> = await this.model.getAll();
-            ctx.body = result[0];
-        });
-    }
+			await next();
+		});
+	}
 
-    public actionGet () {
-        this.get('/api/user/:username', async(ctx: ContextStats, next: any) => {
-            let result: Array<object> = await this.model.findByUserName(ctx.params.username);
+	public actionLogout() {
+		this.get('/auth/logout', bodyParser(), async (ctx) => {
+			if (ctx.isAuthenticated()) {
+				await ctx.cookies.set('token', '');
 
-            if (result) {
-                ctx.body = result[0];
-            } else {
-                ctx.status = ErrorStatus.NoContent;
-            }
-        });
-    }
+				return ctx.logout();
+			} else {
+				ctx.body = {success: false};
+				ctx.throw(401);
+			}
+		});
+	}
 
-    public actionChange () {
-        this.put('/api/user/edit', koaBody, checkAuth, async <RequestStats> (ctx: ContextStats, next: any) => {
-            ctx.status = ErrorStatus.NoContent;
-            const username = ctx.body.user.username;
-            await this.model.update(username, ctx.request.body);
-        })
-    }
+	public actionGetAll() {
+		this.get('/api/users', async (ctx: Context, next: Next) => {
+			const result: Array<object> = await this.model.getAll();
+			ctx.body = result[0];
+		});
+	}
 
-    public actionDelete () {
-        this.delete('/api/user/delete', koaBody, checkAuth, async (ctx: ContextStats, next: any) => {
-            ctx.status = ErrorStatus.NoContent;
-            const username = ctx.body.user.username;
-            await this.model.remove(username);
-        })
-    }
+	public actionGet() {
+		this.get('/api/user/:username', async (ctx: Context, next: Next) => {
+			const result: IUser = await this.model.findByUserName(ctx.params.username);
 
-    public setToken () {
-        this.post('/api/user/setToken', koaBody, async (ctx, next) => {
-           await ctx.cookies.set('token', ctx.request.body.token, { httpOnly: false });
-        })
-    }
+			if (result) {
+				ctx.body = result[0];
+			} else {
+				ctx.status = ErrorStatus.NoContent;
+			}
 
-    public deleteToken () {
-        this.delete('/api/user/deleteToken', async (ctx, next) => {
-           await ctx.cookies.set('token', null);
-        })
-    }
+			await next();
+		});
+	}
 
-    public getToken () {
-        this.get('/api/user/getToken', async (ctx, next) => {
-           ctx.body = {
-               token: ctx.cookies.get('token')
-           }
-        })
-    }
+	public actionChange() {
+		this.put('/api/user/edit', koaBody, checkAuth, async <RequestStats>(ctx: Context, next: Next) => {
+			ctx.status = ErrorStatus.NoContent;
+			const username = ctx.body.user.username;
+			await this.model.update(username, ctx.request.body);
+
+			await next();
+		});
+	}
+
+	public actionDelete() {
+		this.delete('/api/user/delete', koaBody, checkAuth, async (ctx: Context, next: Next) => {
+			ctx.status = ErrorStatus.NoContent;
+			const username = ctx.body.user.username;
+			await this.model.remove(username);
+
+			await next();
+		});
+	}
+
+	public setToken() {
+		this.post('/api/user/setToken', koaBody, async (ctx: Context, next: Next) => {
+			await ctx.cookies.set('token', ctx.request.body.token, {httpOnly: false});
+
+			await next();
+		});
+	}
+
+	public deleteToken() {
+		this.delete('/api/user/deleteToken', async (ctx: Context, next: Next) => {
+			await ctx.cookies.set('token', '');
+
+			await next();
+		});
+	}
+
+	public getToken() {
+		this.get('/api/user/getToken', async (ctx: Context, next: Next) => {
+			ctx.body = {
+				token: ctx.cookies.get('token'),
+			};
+
+			await next();
+		});
+	}
 
 }
 
