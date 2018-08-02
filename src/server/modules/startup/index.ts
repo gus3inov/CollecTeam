2acf6fc0597b9d7cd014df059c28b607a96d7dd9
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-02 22:59:46 +0300

    @create(init modules) adding init modules export

---
+++ src/server/modules/startup/index.ts
@@ -0,0 +1,10 @@
+import StartupController from '@server/modules/startup/StartupController';
+import StartupModel from '@server/modules/startup/StartupModel';
+
+const model = new StartupModel();
+const controller = new StartupController(model);
+
+export default (app: any) => {
+	app.use(controller.getRoutes());
+	app.use(controller.getMethods());
+}
