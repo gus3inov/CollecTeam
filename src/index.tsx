69ac2194ea67b3d2c822b097160969425873d78e
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-03 23:18:04 +0300

    @chore(index)

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,15 @@ import * as ReactDOM from 'react-dom';
 
 import './client/css/bootstrap.css';
 import './client/css/main.css';
-import {ConnectedRouter} from 'react-router-redux';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './client/redux';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {renderRoutes} from 'react-router-config';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import routes from './client/components/routes';
 import { CookiesProvider } from 'react-cookie';
-import { getUniversalCookies } from './client/helpers/cookies'
-import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
-import {deepPurple, blue, grey} from '@material-ui/core/colors';
+import { getUniversalCookies } from './client/helpers/cookies';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { deepPurple, blue, grey } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
     palette: {
