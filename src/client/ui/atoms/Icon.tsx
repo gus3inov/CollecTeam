be7a0d6e952615e1fef9e45298d1eeea7598ac69
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-05 23:42:50 +0300

    @refactor(Icon)

---
+++ src/client/ui/atoms/Icon.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export interface IProps {
+	type: string;
+	width: string;
+	height: string;
+}
+
+const IconStyle = styled.i`
+    display: block;
+    width: ${ props => props.theme.width };
+    height: ${ props => props.theme.height };
+    background-image: url(../assets/img/icons/${ props => props.theme.type }.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+
+const Icon: React.SFC<IProps> = (props: IProps) => {
+	const {
+		type,
+		width,
+		height,
+	} = props;
+
+	const theme = {
+		width,
+		height,
+		type,
+	};
+
+	return (
+		<IconStyle
+			theme={ theme }
+		/>
+	);
+};
+
+export default Icon;
