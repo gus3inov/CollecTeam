75a740a16f42da480f434e1784f0a6c9893ddfaf
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-05 23:41:11 +0300

    @refactore(InputSearch)

--- src/client/ui/atoms/InputSearch.tsx
+++ src/client/ui/atoms/InputSearch.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
-
 import styled from 'styled-components';
+import AntInput from 'antd/lib/input';
 
-const StyledSearchInput = styled.input`
+const StyledSearchInput = styled(AntInput)`
     
-`
+`;
 
-type SearchInputProps = {
+type IProps = {
 
-}
+};
 
-const SearchInput: React.SFC<SearchInputProps> = (props) => {
-    return (
-        <StyledSearchInput placeholder="Поиск..." type="text" className="input input_search"/>
-    )
+const SearchInput: React.SFC<IProps> = (props) => {
+	return (
+		<StyledSearchInput placeholder="Поиск..." type="text"/>
+	);
 };
 
 export default SearchInput;
