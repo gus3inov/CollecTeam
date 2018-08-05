4622a9f917f01e47f5bbbe98ebea70422ed32db0
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-05 23:37:42 +0300

    @refactore(CardHeader) remove export file

--- src/client/ui/molecules/CardHeader/index.tsx
+++ src/client/ui/molecules/CardHeader/index.tsx
@@ -1,3 +1,25 @@
-import CardHeader from './CardHeader';
+import * as React from 'react';
+import ButtonIcon from '@ui/atoms/ButtonIcon';
+
+import StyledCardHeader from './style';
+
+export interface CardHeaderProps {
+	title: string;
+}
+
+const CardHeader: React.SFC<CardHeaderProps> = (props) => {
+	const { title } = props;
+
+	return (
+		<StyledCardHeader>
+			<h2>
+				{title}
+			</h2>
+			<ButtonIcon iconType="plus" onClick={() => console.log('open')}>
+				Open
+			</ButtonIcon>
+		</StyledCardHeader>
+	);
+};
 
 export default CardHeader;
