fdd746f497a2aea7836f47c26c00b60b0cc81d71
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-07 23:22:01 +0300

    @refactore(ButtonIcon) change import

--- src/client/ui/atoms/ButtonIcon.tsx
+++ src/client/ui/atoms/ButtonIcon.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import styled from 'styled-components';
 // import { darkColor, lightColor } from '@ui/theme';
-import AntButton from 'antd/lib/button';
-import AntIcon from 'antd/lib/icon';
+import {
+	Icon as AntIcon,
+	Button as AntButton,
+} from 'antd';
 
-interface IProps {
+type IProps = {
 	children: string;
 	size?: string;
 	type?: string;
 	iconType: string;
 	onClick(): void;
-}
+};
 
 const StyledButton: any = styled(AntButton)`
-	
 `;
 
 const ButtonIcon: React.SFC<IProps> = (props) => {
