2c8d69e2b8ca3c1dce90032753ddcba34cd7e9a7
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-20 23:45:18 +0300

    @refactor(Home)

--- src/client/ui/templates/Home.tsx
+++ src/client/ui/templates/Home.tsx
@@ -1,35 +1,38 @@
 import * as React from 'react';
 import {Fragment} from 'react';
 
-import toggleOpen, { InjectedProps } from '../../hocs/toggleOpen';
+import toggleOpen from '../../hocs/toggleOpen';
 import Menu from '../organisms/Menu';
 import Header from '../organisms/Header';
 import Workspace from '../organisms/Workspace';
 
 export interface HomeProps {
-    children: any;
-    routes: Array<object>;
-    user: object;
+	children: Array<JSX.Element>;
+	
+	isOpen: boolean;
+
+	routes: Array<IMenuRoutes>;
+
+	toggleOpen<A>(): A;
 }
 
-@toggleOpen
-class Home extends React.Component <HomeProps & InjectedProps> {
+class Home extends React.Component <HomeProps, {}> {
 
-    render() {
-        const { toggleOpen, isOpen, children, routes, user } = this.props;
+	render() {
+		const {isOpen, children, routes } = this.props;
 
-        return (
-            <Fragment>
-                <main className="home">
-                    <Header user={user} isOpen={isOpen} />
-                    <Menu routes={routes} toggleOpen={toggleOpen} isOpen={isOpen} />
-                    <Workspace isOpen={isOpen}>
-                        {children}
-                    </Workspace>
-                </main>
-            </Fragment>
-        )
-    }
+		return (
+			<Fragment>
+				<main className="home">
+					<Header isOpen={isOpen} />
+					<Menu routes={routes} toggleOpen={this.props.toggleOpen} isOpen={isOpen}/>
+					<Workspace isOpen={isOpen}>
+						{children}
+					</Workspace>
+				</main>
+			</Fragment>
+		);
+	}
 }
 
-export default Home;
+export default toggleOpen(Home);
