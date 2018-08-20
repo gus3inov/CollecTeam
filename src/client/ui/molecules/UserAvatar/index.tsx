75fac05808531b77284f56cb737a563dcfe77f29
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-20 23:44:52 +0300

    @refactor(UserAvatar)

--- src/client/ui/molecules/UserAvatar/index.tsx
+++ src/client/ui/molecules/UserAvatar/index.tsx
@@ -8,78 +8,81 @@ import green from '@material-ui/core/colors/green';
 import MailIcon from '@material-ui/icons/Mail';
 import Badge from '@material-ui/core/Badge';
 
-import StyledUserAvatar, { styles } from './style';
+import StyledUserAvatar, {styles} from './style';
 import AuthService from '../../../services/AuthService';
 import {withStyles} from '@material-ui/core/styles';
 import {withRouter} from 'react-router';
 
 export interface UserAvatarProps {
-
+	user: any;
+	classes: any;
+	match: any;
+	history: any;
+	location: any;
+	toggleDrawer(): any;
 }
 
-@withStyles(styles)
-@withRouter
 class UserAvatar extends React.Component<UserAvatarProps, any> {
-    state = {
-        anchorEl: null,
-    };
+	state = {
+		anchorEl: null,
+	};
 
-    handleMenu = event => {
-        this.setState({anchorEl: event.currentTarget});
-    };
+	handleMenu = (event: any) => {
+		this.setState({anchorEl: event.currentTarget});
+	};
 
-    logout = () => {
-        const {history} = this.props;
-        AuthService.deauthenticateUser().then(res => {
-            history.push('/login')
-        });
-        this.handleClose();
-    };
+	logout = () => {
+		const {history} = this.props;
+		AuthService.deauthenticateUser().then(res => {
+			history.push('/login');
+		});
+		this.handleClose();
+	};
 
-    handleClose = () => {
-        this.setState({anchorEl: null});
-    };
+	handleClose = () => {
+		this.setState({anchorEl: null});
+	};
 
-    render() {
-        const { user, classes, toggleDrawer } = this.props;
-        const {anchorEl} = this.state;
-        const open = Boolean(anchorEl);
-        return (
-            <StyledUserAvatar className="alt-header__user">
-                <div className="alt-header__user">
-                    <div className="alt-header__user_assignment">
-                        <Badge className={classes.margin} badgeContent={10} color="secondary">
-                            <MailIcon fontSize={13} onClick={toggleDrawer}/>
-                        </Badge>
-                    </div>
-                    <Avatar
-                        className={classes.bigAvatar}
-                        onClick={this.handleMenu}
-                    >
-                        {user.first_name[0].toUpperCase()}
-                        {user.last_name[0].toUpperCase()}
-                    </Avatar>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={open}
-                        onClose={this.handleClose}
-                    >
-                        <MenuItem onClick={() => this.props.history.push('/profile')}>Профиль</MenuItem>
-                        <MenuItem onClick={this.logout}>Выйти</MenuItem>
-                    </Menu>
-                </div>
-            </StyledUserAvatar>
-        );
-    }
+	render() {
+		const {user, classes, toggleDrawer} = this.props;
+		const {anchorEl} = this.state;
+		const open = Boolean(anchorEl);
+		return (
+			<StyledUserAvatar className="alt-header__user">
+				<div className="alt-header__user">
+					<div className="alt-header__user_assignment">
+						<Badge className={classes.margin} badgeContent={10} color="secondary">
+							<MailIcon fontSize={13} onClick={toggleDrawer}/>
+						</Badge>
+					</div>
+					<Avatar
+						className={classes.bigAvatar}
+						onClick={this.handleMenu}
+					>
+						{user.first_name[0].toUpperCase()}
+						{user.last_name[0].toUpperCase()}
+					</Avatar>
+					<Menu
+						id="menu-appbar"
+						anchorEl={anchorEl}
+						anchorOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						transformOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						open={open}
+						onClose={this.handleClose}
+					>
+						<MenuItem onClick={() => this.props.history.push('/profile')}>Профиль</MenuItem>
+						<MenuItem onClick={this.logout}>Выйти</MenuItem>
+					</Menu>
+				</div>
+			</StyledUserAvatar>
+		);
+	}
 }
 
-export default UserAvatar;
+export default withStyles(styles)(withRouter(UserAvatar));
