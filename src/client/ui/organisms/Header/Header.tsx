b55033c30d1d8f39198a34715ce32bed828490e5
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-20 23:45:00 +0300

    @refactor(Header)

--- src/client/ui/organisms/Header/Header.tsx
+++ src/client/ui/organisms/Header/Header.tsx
@@ -7,46 +7,46 @@ import {connect} from 'react-redux';
 import NotificationDrawer from '../NotificationDrawer';
 
 export interface HeaderProps {
-    isOpen?: boolean;
+	user: any;
+	isOpen?: boolean;
 }
 
-@connect(state => {
-    return {
-        user: state.auth.user
-    };
-})
 class Header extends React.Component<HeaderProps, {}> {
-    state = {
-        drawerNotif: false
-    };
+	state = {
+		drawerNotif: false,
+	};
 
-    toggleDrawer = () => {
-        this.setState({
-            drawerNotif: !this.state.drawerNotif
-        })
-    };
+	toggleDrawer = () => {
+		this.setState({
+			drawerNotif: !this.state.drawerNotif,
+		});
+	};
 
-    render() {
-        const {isOpen, user } = this.props;
-        const { drawerNotif } = this.state;
+	render() {
+		const {isOpen, user} = this.props;
+		const {drawerNotif} = this.state;
 
-        return (
-            <StyledHeader className={`alt-header ${isOpen ? 'alt-header_open' : 'alt-header_closed'} container-fluid`}>
-                <div className="alt-header__search">
-                    <Search/>
-                </div>
-                {
-                    user !== null ?
-                        <UserAvatar
-                            user={user}
-                            toggleDrawer={this.toggleDrawer}
-                        />
-                        : ''
-                }
-                <NotificationDrawer isOpen={drawerNotif} toggleOpen={this.toggleDrawer}/>
-            </StyledHeader>
-        );
-    }
+		return (
+			<StyledHeader className={`alt-header ${isOpen ? 'alt-header_open' : 'alt-header_closed'} container-fluid`}>
+				<div className="alt-header__search">
+					<Search/>
+				</div>
+				{
+					user !== null ?
+						<UserAvatar
+							user={user}
+							toggleDrawer={this.toggleDrawer}
+						/>
+						: ''
+				}
+				<NotificationDrawer isOpen={drawerNotif} toggleOpen={this.toggleDrawer}/>
+			</StyledHeader>
+		);
+	}
 }
 
-export default Header;
+export default connect((state: any) => {
+	return {
+		user: state.auth.user,
+	};
+})(Header);
