f7499169256b3739f259c6c0127eb707807dcc54
Muslim Guseinov <gusejnov1999@list.ru>
2018-08-31 23:55:00 +0300

    @refactor(FormStartup)

--- src/client/ui/organisms/FormStartup/FormStartup.tsx
+++ src/client/ui/organisms/FormStartup/FormStartup.tsx
@@ -7,15 +7,15 @@ export interface FormStartupProps {
 }
 
 class FormStartup extends React.Component<FormStartupProps, {}> {
-    render() {
-        const { isOpen, toggleOpen, children } = this.props;
+	render() {
+		const { children } = this.props;
 
-        return (
-            <StyledFormStartup className="form-startup">
-                {children}
-            </StyledFormStartup>
-        );
-    }
+		return (
+			<StyledFormStartup className="form-startup">
+				{children}
+			</StyledFormStartup>
+		);
+	}
 }
 
 export default FormStartup;
